6b99b3a53c7a0b57b8498c90bf50570787d0e509
Ivan Potyng <nekto_ellez@icloud.com>
2021-05-24 16:07:34 +0300

    1) Implementation of a color change application. 2) Adding a text box with displaying numbers.

--- RGBApp/ContentView.swift
+++ RGBApp/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView              : View
+{
+    
+    // MARK: компоненты цвета
+    
+    @State var redComponent     : Double    =   0.5
+    @State var greenComponent   : Double    =   0.5
+    @State var blueComponent    : Double    =   0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Slider(value        : $redComponent)
+            Slider(value        : $greenComponent)
+            Slider(value        : $blueComponent)
+            Color(
+                red             : redComponent,
+                green           : greenComponent,
+                blue            : blueComponent)
+            Text("\(redComponent) \(greenComponent) \(blueComponent)")
+        }
+        .padding(.all, 10.0)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView_Previews     : PreviewProvider
+{
+    static var previews         : some View {
+        Group {
+            ContentView()
+                .previewDevice("iPhone 12")
+        }
     }
 }
+
